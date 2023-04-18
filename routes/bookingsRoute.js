8d674249a726aa263ca68199ef21e8940365411b
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-04-18 11:59:20 +0530

    1st

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Booking = require("../models/bookingModel");
-const Equipment = require("../models/bikeModel");
+const Equipment = require("../models/equipmentModel");
 const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(
   "sk_test_51IYnC0SIR2AbPxU0EiMx1fTwzbZXLbkaOcbc2cXx49528d9TGkQVjUINJfUDAnQMVaBFfBDP5xtcHCkZG1n1V3E800U7qXFmGf"
