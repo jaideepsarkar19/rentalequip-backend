604ba1f35f46ae0ef62f82726fea680b276a4fdd
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-04-19 23:55:41 +0530

    2nd

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB(){
 
-    mongoose.connect('mongodb+srv://helpjs19:12345@cluster0.zdcrg5i.mongodb.net/?retryWrites=true&w=majority' )
+    mongoose.connect('mongodb+srv://helpjs19:12345@cluster0.zdcrg5i.mongodb.net/rentalequip?retryWrites=true&w=majority' )
 //, {useUnifiedTopology: true , useNewUrlParser: true}
     const connection = mongoose.connection
 
